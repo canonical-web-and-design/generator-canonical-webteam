a80a33597ca7ed1b03458e73c83573f6fca4afe8
Robin Winslow <robin@robinwinslow.co.uk>
2017-04-10 16:58:04 +0100

    Create or update package.json
    Add the option to create or update the package.json from a basic
    template.
    
    The scripts and dependencies aren't yet intended to be final, just
    placeholders. We expect package.json will be inspected before it is
    committed.

--- generators/run/index.js
+++ generators/run/index.js
@@ -20,6 +20,23 @@ module.exports = class extends Generator {
       );
     }
 
+    if (fs.existsSync(this.destinationPath('package.json'))) {
+      prompts.push(
+        {
+          type    : 'confirm',
+          name    : 'updateDevDependencies',
+          message : 'Should we update "devDependencies" in package.json?'
+        }
+      );
+      prompts.push(
+        {
+          type    : 'confirm',
+          name    : 'updateScripts',
+          message : 'Should we update "scripts" in package.json?'
+        }
+      );
+    }
+
     return this.prompt(prompts).then(
       (answers) => {
         this.answers = answers;
@@ -73,6 +90,45 @@ module.exports = class extends Generator {
           );
         }
       }
+    }
+
+    // package,json
+    if (! fs.existsSync(this.destinationPath('package.json'))) {
+      // Copy default package.json
+      this.fs.copy(
+        this.templatePath('package.json'),
+        this.destinationPath('package.json')
+      );
+    } else if (this.answers.updateDevDependencies || this.answers.updateScripts) {
+      // We're updating package.json, so parse contents of both new and old package.json
+      const currentJson = fs.readFileSync(this.destinationPath('package.json'), 'utf8');
+      const incomingJson = fs.readFileSync(this.templatePath('package.json'), 'utf8');
+      const packageContents = JSON.parse(currentJson);
+      const incomingPackageContents = JSON.parse(incomingJson);
+
+      // Update devDependencies if requested
+      if (this.answers.updateDevDependencies) {
+        console.log('here');
+        const currentDevDependencies = packageContents['devDependencies'] || {};
+        const incomingDevDependencies = incomingPackageContents['devDependencies'];
+        const updatedDevDependencies = util._extend(currentDevDependencies, incomingDevDependencies);
+        packageContents['devDependencies'] = updatedDevDependencies;
+      }
+
+      // Update scripts if requested
+      if (this.answers.updateScripts) {
+        const currentScripts = packageContents['scripts'] || {};
+        const incomingScripts = incomingPackageContents['scripts'];
+        const updatedScripts = util._extend(currentScripts, incomingScripts);
+        packageContents['scripts'] = updatedScripts;
+      }
+
+      const updatedJson = JSON.stringify(packageContents, null, 2);
+      fs.writeFileSync(
+        this.destinationPath('package.json'),
+        updatedJson,
+        'utf8'
+      );
     }
   }
 };
