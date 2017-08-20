a5666b836063afa6d6c4b37661dfd0d2a44d680f
Robin Winslow <robin@robinwinslow.co.uk>
2017-08-20 19:12:55 +0100

    Remove unneeded template context

--- generators/run/index.js
+++ generators/run/index.js
@@ -49,9 +49,6 @@ module.exports = class extends Generator {
   writing() {
     // Defaults
     let options = {
-      'django': false,
-      'db': false,
-      'jekyll': false,
       'version': pjson.version
     };
 
