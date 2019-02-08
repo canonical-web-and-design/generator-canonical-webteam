a936df02acc06ba24ced5116bc01d3fa1b3865c7
Robin Winslow <robin@robinwinslow.co.uk>
2019-02-08 16:03:37 +0000

    Settings for whitenoise v4

--- generators/django/templates/webapp/settings.py
+++ generators/django/templates/webapp/settings.py
@@ -51,6 +51,7 @@ SEARCH_SERVER_URL = 'http://10.22.112.8/search'
 SEARCH_TIMEOUT = 10
 
 MIDDLEWARE_CLASSES = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'unslashed.middleware.RemoveSlashMiddleware',
 ]
 
